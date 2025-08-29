6be855f92065267b1fcb55d60fb5c49e764d8140
hran-8 <90593189+hran-8@users.noreply.github.com>
2025-08-29 18:34:58 -0400

    Update font configuration to use globals.css instead of tailwind config

--- frontend/src/components/ui/Button.tsx
+++ frontend/src/components/ui/Button.tsx
@@ -1,3 +1,5 @@
+
+
 export default function Button() {
-  return <button className={`px-6 py-2 font-[var(--font-annapurna)]`}>Click Me</button>;
+  return <button className={`px-6 py-2 font-special`}>Click me</button>;
 }
