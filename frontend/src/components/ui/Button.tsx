6c4219f02b5ba11624ba485551ee66757183b218
hran-8 <90593189+hran-8@users.noreply.github.com>
2025-09-10 15:25:16 -0400

    Add simple styling to navbar and complete mobile responsiveness

--- frontend/src/components/ui/Button.tsx
+++ frontend/src/components/ui/Button.tsx
@@ -1,10 +1,9 @@
-export default function Button({ children, onClick }: { children?: React.ReactNode, onClick?: React.MouseEventHandler<HTMLButtonElement> }) {
+export default function Button({ children }: { children?: React.ReactNode }) {
   return (
     <button
       className="px-6 py-2 font-special uppercase text-2xl opacity-60 
                  hover:text-white hover:opacity-100 cursor-pointer 
                  transition-colors duration-200"
-      onClick={onClick}
     >
       {children}
     </button>
