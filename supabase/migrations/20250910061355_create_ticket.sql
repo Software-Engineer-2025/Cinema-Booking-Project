cac43c185752e5e3af039a7e30373405ab0954dd
KadeStyron <kjs00988@uga.edu>
2025-09-10 22:57:40 -0400

    updated migrations now there is no theater

--- supabase/migrations/20250910061355_create_ticket.sql
+++ supabase/migrations/20250910061355_create_ticket.sql
@@ -1,13 +1,10 @@
 CREATE TABLE Ticket (
     ticket_id UUID PRIMARY KEY,
-    user_id UUID REFERENCES UserProfile(user_id) DEFAULT NULL,
-    seat TEXT NOT NULL,
-    is_taken BOOLEAN NOT NULL,
-    theater_section_date DATE NOT NULL,
-    theater_section_time TIME NOT NULL,
-    theater_section_screen_number INT NOT NULL,
-    theater_section_name TEXT NOT NULL,
+    seat TEXT,
+    is_taken BOOLEAN DEFAULT FALSE,
     price DOUBLE PRECISION NOT NULL,
-    FOREIGN KEY (theater_section_date, theater_section_time, theater_section_screen_number, theater_section_name)
-        REFERENCES TheaterSection(date, time, screen_number, name)
+    status TEXT DEFAULT 'open',
+    user_id UUID REFERENCES UserProfile(user_id) DEFAULT NULL,
+    theater_section_id UUID REFERENCES TheaterSection(theater_section_id)
+
 );
