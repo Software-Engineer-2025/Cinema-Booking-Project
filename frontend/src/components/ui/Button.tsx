1a3c1a4034a22d23bb9f0ca6f7b9821c726acf0c
hran-8 <90593189+hran-8@users.noreply.github.com>
2025-09-15 11:42:35 -0400

    CB-18
    * Add dropdown to profile and make navbar responsive
    
    * Add simple styling to navbar and complete mobile responsiveness
    
    * Make responsiveness more seamless; navbar doesn't break to 2 lines when resizing
    
    ---------
    
    Co-authored-by: hl89802 <kkuui.sa@gmail.com>

--- frontend/src/components/ui/Button.tsx
+++ frontend/src/components/ui/Button.tsx
@@ -1,5 +1,11 @@
-
-
-export default function Button( { children }: { children?: React.ReactNode } ) {
-  return <button className={`px-6 py-2 font-special uppercase text-2xl opacity-60`}>{children}</button>;
+export default function Button({ children }: { children?: React.ReactNode }) {
+  return (
+    <button
+      className="px-6 py-2 font-special uppercase text-2xl opacity-60 
+                 hover:text-white hover:opacity-100 cursor-pointer 
+                 transition-colors duration-200"
+    >
+      {children}
+    </button>
+  );
 }
